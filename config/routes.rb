df3f02e92ec3c859c6a0cca972630e81da0af2bc
Alex Forey <me@alexforey.com>
2018-04-20 12:05:45 +0100

    Fix small bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+
+  get "/token/:name", to: "screens#show"
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users
   resource :kiosk, only: :show
 
   root to: 'kiosk#show'
-
-  get "/token/:name", to: "screens#show"
 
   get "/:code", to: "kiosk#show"
 
